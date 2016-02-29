c7b65128208761568d03111d91c39a9e3f98005c
Dennis Schulz <dennis@port42.org>
2016-02-29 21:17:48 +0100

    first approach using panning (drag+move)

--- client/components/ConnectThingsAppRootComponent.jsx
+++ client/components/ConnectThingsAppRootComponent.jsx
@@ -13,6 +13,14 @@ ConnectThingsAppRoot = () => {
 Meteor.startup(()=> {
   var metaInfo = {name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui"};
   DocHead.addMeta(metaInfo);
+  DocHead.addMeta({
+    name: "apple-mobile-web-app-status-bar-style",
+    content: "black-translucent"
+  });
+
+  document.ontouchmove = function(event){
+    event.preventDefault();
+  };
 
   ReactDOM.render(
     <ConnectThingsAppRoot />,
--- client/components/Things.jsx
+++ client/components/Things.jsx
@@ -8,24 +8,59 @@ Things = ({ dispatch }) => {
     console.log(foo);
   }
 
-  handleSwipe = (foo) => {
-    console.log(foo);
+  handlePan = (foo) => {
+    var clientY = foo.pointers[0].clientY;
+    var body = document.body, html = document.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight,
+      html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    half_height = height/2;
+    var amount = 0;
+
+    if (clientY > half_height) {
+      amount = clientY - half_height;
+    }
+    else if(clientY < half_height) {
+      amount = (half_height - clientY) * -1;
+    }
+
+    pull_percentage = 100 / half_height * amount;
+
+    if(pull_percentage < 0) {
+      $('.thing-container .bucket-a').css('background-color', '#888888');
+      $('.thing-container .bucket-b').css('background-color', '#333333');
+    }
+    else if(pull_percentage > 0) {
+      $('.thing-container .bucket-b').css('background-color', '#888888');
+      $('.thing-container .bucket-a').css('background-color', '#333333');
+    }
+    else {
+      $('.thing-container .bucket-b').css('background-color', '#333333');
+      $('.thing-container .bucket-a').css('background-color', '#333333');
+    }
   }
+
+  var options = {
+    touchAction:true
+  };
 
   return (
     <div className="thing-container">
-      <div className="bucket">
+      <div className="skip centered">
+        <div className="centered left">&#x25C4;</div>
+        <div className="centered right">&#x25B6;</div>
+      </div>
+      <div className="bucket bucket-a">
         <div className="centered">Bucket A</div>
       </div>
       <div className="fullwidth">
-        <div className="skip">
-        skip
-        </div>
+        <Hammer onTap={handleTap} onPan={handlePan} vertical={true}>
         <div className="word">
-          <Hammer onTap={handleTap} onSwipe={handleSwipe}><div className="centered">Some word</div></Hammer>
+          <div className="centered">Some word</div>
         </div>
+        </Hammer>
       </div>
-      <div className="bucket">
+      <div className="bucket bucket-b">
         <div className="centered">Bucket B</div>
       </div>
     </div>
